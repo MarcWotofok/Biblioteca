f129989b98a725dc8b30a0f7e2c8b9221c4fdae7
mgonzalezmesa <33700123+mgonzalezmesa@users.noreply.github.com>
2017-11-20 11:08:13 +0100

    Add files via upload

--- vistas/IntAdmin.php
+++ vistas/IntAdmin.php
@@ -1,6 +1,8 @@
+<link rel="stylesheet" href="css/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="css/estilo.css" media="screen" />
 <?php
         
-        echo "<table border='1' cellspacing='0'>";
+        echo "<table border='1' cellspacing='0' style='width: 90%;margin: 0 auto;text-align: center'>";
                 echo "<tr>
                         <td>id</td>
                         <td>Titulo</td>
@@ -9,12 +11,13 @@
                         <td>Lugar de edicion</td>
                         <td>Fecha de edicion</td>
                         <td>ISBN</td>
-                        <td>tipo</td>
+                        <td>Disponibilidad</td>
+                        <td colspan='3'><a href='index.php?accion=showInsertLibro'><i class='fa fa-plus' aria-hidden='true'></i><i class='fa fa-book' aria-hidden='true'></i></a></td>
                        </tr>";
 
 
             $tablaUsuario = $datos["tabla"];
-			echo "<a href='index.php?accion=showInsertLibro'>Insertar</a>";
+			echo "<a href='index.php?accion=showInsertLibro'><i class='fa fa-plus' aria-hidden='true'></i><i class='fa fa-book' aria-hidden='true'></i></a>";
             foreach ($tablaUsuario as $usu) {
                 echo "<tr>
                         <td>".$usu[0]."</td>
@@ -25,9 +28,9 @@
                         <td>".$usu[5]."</td>
                         <td>".$usu[6]."</td>
                         <td>".$usu[7]."</td>
-                        <td><a href='index.php?accion=deleteLibro&id=".$usu[0]."'></a></td>
-                        <td><a href='index.php?accion=modificarLibro&id=".$usu[0]."'>Modificar</a></td>
-                        <td><a href='index.php?accion=showInsertImg&id=".$usu[0]."'>Insertar Pagina</a></td>
+                        <td><a href='index.php?accion=modificarLibro&id=".$usu[0]."'><i class='fa fa-pencil-square-o' aria-hidden='true'></i></a></td>
+                        <td><a href='index.php?accion=showInsertImg&id=".$usu[0]."'><i class='fa fa-file-image-o' aria-hidden='true'></i></a></td>
+                        <td><a href='index.php?accion=deleteLibro&id=".$usu[0]."'><i class='fa fa-trash' aria-hidden='true'></i></a></td>
                        </tr>";
             }
             echo "</table>";
